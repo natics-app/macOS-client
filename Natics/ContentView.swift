3b92860f45ec4e5d2675fc749e572d1b552f6c01
Puras Handharmahua <phandharmahua@gmail.com>
2021-10-26 23:56:08 +0700

    no message

---
+++ Natics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Natics
+//
+//  Created by Puras Handharmahua on 26/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Natics/NaticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaticsApp.swift
+//  Natics
+//
+//  Created by Puras Handharmahua on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NaticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
