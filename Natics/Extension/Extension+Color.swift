d9476540ea9069377840a45b9c7aeff56b1fb035
Puras Handharmahua <phandharmahua@gmail.com>
2021-11-01 12:14:54 +0700

    Add color set Add extension for Color Add constants for endpoint

---
+++ Natics/Extension/Extension+Color.swift
@@ -0,0 +1,20 @@
+//
+//  Extension+Color.swift
+//  Natics
+//
+//  Created by Puras Handharmahua on 01/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let colorTheme = MCColorTheme()
+}
+
+struct MCColorTheme {
+    let MCMainGreen = Color("MCMainGreen", bundle: nil)
+    let MCDarkGreen = Color("MCDarkGreen", bundle: nil)
+    let MCLightGreen = Color("MCLightGreen", bundle: nil)
+    let MCWindowBG = Color("MCWindowBG", bundle: nil)
+}
--- Natics/NaticsApp.swift
+++ Natics/NaticsApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct NaticsApp: App {
+    
+//    let clr = NSColor.labe
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
