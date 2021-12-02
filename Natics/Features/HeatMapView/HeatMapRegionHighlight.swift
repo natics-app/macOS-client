fcdedba2082a4edd046916b63a68e1d3dadfd572
Jackie Leonardy <61610003+jajackleon@users.noreply.github.com>
2021-12-02 09:58:12 +0700

    Feature/mac 74 (#95)
    * map hovering
    
    * Mouse overview

---
+++ Natics/Features/HeatMapView/HeatMapRegionHighlight.swift
@@ -0,0 +1,23 @@
+//
+//  HeatMapRegionHighlight.swift
+//  Natics
+//
+//  Created by Jackie Leonardy on 29/11/21.
+//
+
+import SwiftUI
+
+struct HeatMapRegionHighlight: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }.frame(width: 200, height: 140)
+            .background(Color.orange)
+    }
+}
+
+struct HeatMapRegionHighlight_Previews: PreviewProvider {
+    static var previews: some View {
+        HeatMapRegionHighlight()
+    }
+}
