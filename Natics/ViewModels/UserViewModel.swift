1d6cd34cec147b130ae1611d73480d36a3d97bc5
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-12-05 14:30:13 +0700

    add user view model; (#100)

---
+++ Natics/ViewModels/UserViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserViewModel.swift
+//  Natics
+//
+//  Created by Puras Handharmahua on 05/12/21.
+//
+
+import Foundation
+import SwiftUI
+
+class UserViewModel {
+    
+    // MARK: PUBLISHED PROPERTIES
+    @Published var username: String = ""
+    
+    // MARK: PUBLIC PROPERTIES
+    var token: String = ""
+    
+}
