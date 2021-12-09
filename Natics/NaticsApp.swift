ac96339086de6d1b44ac53c96c744a597aa81621
EcoRanger <adiputrachristian17@gmail.com>
2021-12-09 17:44:04 +0700

    Setup Notif

--- Natics/NaticsApp.swift
+++ Natics/NaticsApp.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import AppKit
 import Combine
+import PushNotifications
+//import PusherSwift
 
 @main
 struct NaticsApp: App {
@@ -35,5 +37,30 @@ struct NaticsApp: App {
             SidebarCommands()
             ToolbarCommands()
         }
+    }
+}
+
+//@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate{
+    
+    let pushNotification = PushNotifications.shared
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        self.pushNotification.start(instanceId: "44ad15c6-2c62-4c42-9ccb-6056fcb55849")
+        self.pushNotification.registerForRemoteNotifications()
+        try? self.pushNotification.addDeviceInterest(interest: "debug-test")
+    }
+    
+    func application(_ application: NSApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        self.pushNotification.registerDeviceToken(deviceToken)
+    }
+    
+    func application(_ application: NSApplication, didReceiveRemoteNotification userInfo: [String : Any]) {
+        self.pushNotification.handleNotification(userInfo: userInfo)
+        print(userInfo)
+    }
+    
+    func application(_ application: NSApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Remote notification support is unavailable due to error: \(error.localizedDescription)")
     }
 }
